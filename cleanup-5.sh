b0a86c0ed4a75bfe552e216c30dfd08b5be46de2
wilderlavington <wilderlavington@gmail.com>
2022-09-22 19:25:18 -0700

    .

--- cleanup-5.sh
+++ cleanup-5.sh
@@ -10,67 +10,67 @@ pwd
 conda activate ubcml
 if [ $SLURM_ARRAY_TASK_ID -eq 0 ]
 then
-    python main.py --min_epochs=500 --algo=Ada_FMDOpt --normalize_epochs_lengths=1 --seed=1 --fullbatch=1 --m=1 --use_optimal_stepsize=1 --loss=MSELoss --dataset_name=rcv1 --eta_schedule=constant --group=narval
+    python main.py --min_epochs=500 --algo=Ada_FMDOpt --normalize_epochs_lengths=1 --seed=1 --fullbatch=1 --m=1 --use_optimal_stepsize=1 --loss=BCEWithLogitsLoss --dataset_name=rcv1 --eta_schedule=constant --group=narval
 fi
 if [ $SLURM_ARRAY_TASK_ID -eq 1 ]
 then
-    python main.py --min_epochs=500 --algo=Ada_FMDOpt --normalize_epochs_lengths=1 --seed=2 --fullbatch=1 --m=1 --use_optimal_stepsize=1 --loss=MSELoss --dataset_name=rcv1 --eta_schedule=constant --group=narval
+    python main.py --min_epochs=500 --algo=Ada_FMDOpt --normalize_epochs_lengths=1 --seed=2 --fullbatch=1 --m=1 --use_optimal_stepsize=1 --loss=BCEWithLogitsLoss --dataset_name=rcv1 --eta_schedule=constant --group=narval
 fi
 if [ $SLURM_ARRAY_TASK_ID -eq 2 ]
 then
-    python main.py --min_epochs=500 --algo=Ada_FMDOpt --normalize_epochs_lengths=1 --seed=3 --fullbatch=1 --m=1 --use_optimal_stepsize=1 --loss=MSELoss --dataset_name=rcv1 --eta_schedule=constant --group=narval
+    python main.py --min_epochs=500 --algo=Ada_FMDOpt --normalize_epochs_lengths=1 --seed=3 --fullbatch=1 --m=1 --use_optimal_stepsize=1 --loss=BCEWithLogitsLoss --dataset_name=rcv1 --eta_schedule=constant --group=narval
 fi
 
 if [ $SLURM_ARRAY_TASK_ID -eq 3 ]
 then
-    python main.py --min_epochs=500 --algo=Ada_FMDOpt --normalize_epochs_lengths=1 --seed=1 --fullbatch=1 --m=2 --use_optimal_stepsize=1 --loss=MSELoss --dataset_name=rcv1 --eta_schedule=constant --group=narval
+    python main.py --min_epochs=500 --algo=Ada_FMDOpt --normalize_epochs_lengths=1 --seed=1 --fullbatch=1 --m=2 --use_optimal_stepsize=1 --loss=BCEWithLogitsLoss --dataset_name=rcv1 --eta_schedule=constant --group=narval
 fi
 if [ $SLURM_ARRAY_TASK_ID -eq 4 ]
 then
-    python main.py --min_epochs=500 --algo=Ada_FMDOpt --normalize_epochs_lengths=1 --seed=2 --fullbatch=1 --m=2 --use_optimal_stepsize=1 --loss=MSELoss --dataset_name=rcv1 --eta_schedule=constant --group=narval
+    python main.py --min_epochs=500 --algo=Ada_FMDOpt --normalize_epochs_lengths=1 --seed=2 --fullbatch=1 --m=2 --use_optimal_stepsize=1 --loss=BCEWithLogitsLoss --dataset_name=rcv1 --eta_schedule=constant --group=narval
 fi
 if [ $SLURM_ARRAY_TASK_ID -eq 5 ]
 then
-    python main.py --min_epochs=500 --algo=Ada_FMDOpt --normalize_epochs_lengths=1 --seed=3 --fullbatch=1 --m=2 --use_optimal_stepsize=1 --loss=MSELoss --dataset_name=rcv1 --eta_schedule=constant --group=narval
+    python main.py --min_epochs=500 --algo=Ada_FMDOpt --normalize_epochs_lengths=1 --seed=3 --fullbatch=1 --m=2 --use_optimal_stepsize=1 --loss=BCEWithLogitsLoss --dataset_name=rcv1 --eta_schedule=constant --group=narval
 fi
 
 if [ $SLURM_ARRAY_TASK_ID -eq 6 ]
 then
-    python main.py --min_epochs=500 --algo=Ada_FMDOpt --normalize_epochs_lengths=1 --seed=1 --fullbatch=1 --m=5 --use_optimal_stepsize=1 --loss=MSELoss --dataset_name=rcv1 --eta_schedule=constant --group=narval
+    python main.py --min_epochs=500 --algo=Ada_FMDOpt --normalize_epochs_lengths=1 --seed=1 --fullbatch=1 --m=5 --use_optimal_stepsize=1 --loss=BCEWithLogitsLoss --dataset_name=rcv1 --eta_schedule=constant --group=narval
 fi
 if [ $SLURM_ARRAY_TASK_ID -eq 7 ]
 then
-    python main.py --min_epochs=500 --algo=Ada_FMDOpt --normalize_epochs_lengths=1 --seed=2 --fullbatch=1 --m=5 --use_optimal_stepsize=1 --loss=MSELoss --dataset_name=rcv1 --eta_schedule=constant --group=narval
+    python main.py --min_epochs=500 --algo=Ada_FMDOpt --normalize_epochs_lengths=1 --seed=2 --fullbatch=1 --m=5 --use_optimal_stepsize=1 --loss=BCEWithLogitsLoss --dataset_name=rcv1 --eta_schedule=constant --group=narval
 fi
 if [ $SLURM_ARRAY_TASK_ID -eq 8 ]
 then
-    python main.py --min_epochs=500 --algo=Ada_FMDOpt --normalize_epochs_lengths=1 --seed=3 --fullbatch=1 --m=5 --use_optimal_stepsize=1 --loss=MSELoss --dataset_name=rcv1 --eta_schedule=constant --group=narval
+    python main.py --min_epochs=500 --algo=Ada_FMDOpt --normalize_epochs_lengths=1 --seed=3 --fullbatch=1 --m=5 --use_optimal_stepsize=1 --loss=BCEWithLogitsLoss --dataset_name=rcv1 --eta_schedule=constant --group=narval
 fi
 
 if [ $SLURM_ARRAY_TASK_ID -eq 9 ]
 then
-    python main.py --min_epochs=500 --algo=Ada_FMDOpt --normalize_epochs_lengths=1 --seed=1 --fullbatch=1 --m=10 --use_optimal_stepsize=1 --loss=MSELoss --dataset_name=rcv1 --eta_schedule=constant --group=narval
+    python main.py --min_epochs=500 --algo=Ada_FMDOpt --normalize_epochs_lengths=1 --seed=1 --fullbatch=1 --m=10 --use_optimal_stepsize=1 --loss=BCEWithLogitsLoss --dataset_name=rcv1 --eta_schedule=constant --group=narval
 fi
 if [ $SLURM_ARRAY_TASK_ID -eq 10 ]
 then
-    python main.py --min_epochs=500 --algo=Ada_FMDOpt --normalize_epochs_lengths=1 --seed=2 --fullbatch=1 --m=10 --use_optimal_stepsize=1 --loss=MSELoss --dataset_name=rcv1 --eta_schedule=constant --group=narval
+    python main.py --min_epochs=500 --algo=Ada_FMDOpt --normalize_epochs_lengths=1 --seed=2 --fullbatch=1 --m=10 --use_optimal_stepsize=1 --loss=BCEWithLogitsLoss --dataset_name=rcv1 --eta_schedule=constant --group=narval
 fi
 if [ $SLURM_ARRAY_TASK_ID -eq 11 ]
 then
-    python main.py --min_epochs=500 --algo=Ada_FMDOpt --normalize_epochs_lengths=1 --seed=3 --fullbatch=1 --m=10 --use_optimal_stepsize=1 --loss=MSELoss --dataset_name=rcv1 --eta_schedule=constant --group=narval
+    python main.py --min_epochs=500 --algo=Ada_FMDOpt --normalize_epochs_lengths=1 --seed=3 --fullbatch=1 --m=10 --use_optimal_stepsize=1 --loss=BCEWithLogitsLoss --dataset_name=rcv1 --eta_schedule=constant --group=narval
 fi
 
 if [ $SLURM_ARRAY_TASK_ID -eq 12 ]
 then
-    python main.py --min_epochs=500 --algo=Ada_FMDOpt --normalize_epochs_lengths=1 --seed=1 --fullbatch=1 --m=20 --use_optimal_stepsize=1 --loss=MSELoss --dataset_name=rcv1 --eta_schedule=constant --group=narval
+    python main.py --min_epochs=500 --algo=Ada_FMDOpt --normalize_epochs_lengths=1 --seed=1 --fullbatch=1 --m=20 --use_optimal_stepsize=1 --loss=BCEWithLogitsLoss --dataset_name=rcv1 --eta_schedule=constant --group=narval
 fi
 if [ $SLURM_ARRAY_TASK_ID -eq 13 ]
 then
-    python main.py --min_epochs=500 --algo=Ada_FMDOpt --normalize_epochs_lengths=1 --seed=2 --fullbatch=1 --m=20 --use_optimal_stepsize=1 --loss=MSELoss --dataset_name=rcv1 --eta_schedule=constant --group=narval
+    python main.py --min_epochs=500 --algo=Ada_FMDOpt --normalize_epochs_lengths=1 --seed=2 --fullbatch=1 --m=20 --use_optimal_stepsize=1 --loss=BCEWithLogitsLoss --dataset_name=rcv1 --eta_schedule=constant --group=narval
 fi
 if [ $SLURM_ARRAY_TASK_ID -eq 14 ]
 then
-    python main.py --min_epochs=500 --algo=Ada_FMDOpt --normalize_epochs_lengths=1 --seed=3 --fullbatch=1 --m=20 --use_optimal_stepsize=1 --loss=MSELoss --dataset_name=rcv1 --eta_schedule=constant --group=narval
+    python main.py --min_epochs=500 --algo=Ada_FMDOpt --normalize_epochs_lengths=1 --seed=3 --fullbatch=1 --m=20 --use_optimal_stepsize=1 --loss=BCEWithLogitsLoss --dataset_name=rcv1 --eta_schedule=constant --group=narval
 fi
 
 exit

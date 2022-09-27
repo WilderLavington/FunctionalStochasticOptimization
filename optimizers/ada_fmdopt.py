major numerical error fix

optimizers/ada_fmdopt.py
@@ -47,7 +47,7 @@ class Ada_FMDOpt(SGD_FMDOpt):
 
         # set  eta schedule
         eta = self.eta * (self.grad_sum).pow(0.5)
-        print(eta, self.inner_optim.state['step_size'])
+
         # construct surrogate-loss to optimize (avoids extra backward calls)
         def surrogate(call_backward=True):
             #
@@ -56,16 +56,17 @@ class Ada_FMDOpt(SGD_FMDOpt):
             loss, f, inner_closure = closure(call_backward=False)
             # m by d -> 1
             loss = torch.sum(dlt_dft*f)
-            # remove cap F
+            # remove cap F 
             reg_term = self.div_op(f,f_t.detach())
             # compute full surrogate
-            surr = (loss + eta * reg_term) / batch_size
+            surr = (loss / eta + reg_term) / batch_size
             # do we differentiate
             if call_backward:
                 surr.backward()
             # return loss
             return surr
 
+        # ok.
         if self.reset_lr_on_step:
             self.inner_optim.state['step_size'] = self.init_step_size
 

optimizers/sgd_fmdopt.py
@@ -119,7 +119,7 @@ class SGD_FMDOpt(torch.optim.Optimizer):
             # remove cap F
             reg_term = self.div_op(f,f_t.detach())
             # compute full surrogate
-            surr = (loss + eta * reg_term) / batch_size
+            surr = (loss / eta + reg_term) / batch_size
             # do we differentiate
             if call_backward:
                 surr.backward()

optimizers/sls_fmdopt.py
@@ -34,7 +34,6 @@ class SLS_FMDOpt(SGD_FMDOpt):
         for i in range(100):
             lhs = inner_closure(f_t - (1/eta_prop) * dlt_dft)
             rhs = inner_closure(f_t) - (1/eta_prop) * self.c * torch.norm(dlt_dft).pow(2)
-            print(lhs - rhs, eta_prop)
             if lhs < rhs:
                 eta_prop /= self.beta_update
             else:
@@ -81,7 +80,7 @@ class SLS_FMDOpt(SGD_FMDOpt):
             # remove cap F
             reg_term = self.div_op(f,f_t.detach())
             # compute full surrogate
-            surr = (loss + eta * reg_term) / batch_size
+            surr = (loss / eta + reg_term) / batch_size
             # do we differentiate
             if call_backward:
                 surr.backward()
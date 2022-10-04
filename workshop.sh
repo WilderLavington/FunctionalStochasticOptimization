8f700c7af9c21069ce1fd5469c1f461b67914d74
wilderlavington <wilderlavington@gmail.com>
2022-10-04 01:48:07 -0700

    .

--- workshop.sh
+++ workshop.sh
@@ -30,7 +30,12 @@ python manual_sweep.py --yaml_file=/home/wlavington/Desktop/FunctionalStochastic
 python manual_sweep.py --yaml_file=/home/wlavington/Desktop/FunctionalStochasticOptimization/configs/workshop/figure_6/optimal_stepsize/minibatch/baselines.yaml --job_name=fig_1d
 python manual_sweep.py --yaml_file=/home/wlavington/Desktop/FunctionalStochasticOptimization/configs/workshop/figure_6/optimal_stepsize/minibatch/funcopt.yaml --job_name=fig_1d
 
-python manual_sweep.py --yaml_file=/home/wlavington/Desktop/FunctionalStochasticOptimization/configs/workshop/figure_6/optimal_stepsize/fullbatch/baselines.yaml --job_name=fig_1e
-python manual_sweep.py --yaml_file=/home/wlavington/Desktop/FunctionalStochasticOptimization/configs/workshop/figure_6/optimal_stepsize/fullbatch/funcopt.yaml --job_name=fig_1e
-python manual_sweep.py --yaml_file=/home/wlavington/Desktop/FunctionalStochasticOptimization/configs/workshop/figure_6/optimal_stepsize/minibatch/baselines.yaml --job_name=fig_1e
-python manual_sweep.py --yaml_file=/home/wlavington/Desktop/FunctionalStochasticOptimization/configs/workshop/figure_6/optimal_stepsize/minibatch/funcopt.yaml --job_name=fig_1e
+python manual_sweep.py --yaml_file=/home/wlavington/Desktop/FunctionalStochasticOptimization/configs/workshop/figure_7/optimal_stepsize/fullbatch/baselines.yaml --job_name=fig_1e
+python manual_sweep.py --yaml_file=/home/wlavington/Desktop/FunctionalStochasticOptimization/configs/workshop/figure_7/optimal_stepsize/fullbatch/funcopt.yaml --job_name=fig_1e
+python manual_sweep.py --yaml_file=/home/wlavington/Desktop/FunctionalStochasticOptimization/configs/workshop/figure_7/optimal_stepsize/minibatch/baselines.yaml --job_name=fig_1e
+python manual_sweep.py --yaml_file=/home/wlavington/Desktop/FunctionalStochasticOptimization/configs/workshop/figure_7/optimal_stepsize/minibatch/funcopt.yaml --job_name=fig_1e
+
+python manual_sweep.py --yaml_file=/home/wlavington/Desktop/FunctionalStochasticOptimization/configs/workshop/figure_8/gridsearch/fullbatch/baselines.yaml --job_name=fig_2
+python manual_sweep.py --yaml_file=/home/wlavington/Desktop/FunctionalStochasticOptimization/configs/workshop/figure_8/gridsearch/fullbatch/funcopt.yaml --job_name=fig_2
+python manual_sweep.py --yaml_file=/home/wlavington/Desktop/FunctionalStochasticOptimization/configs/workshop/figure_8/gridsearch/minibatch/baselines.yaml --job_name=fig_2
+python manual_sweep.py --yaml_file=/home/wlavington/Desktop/FunctionalStochasticOptimization/configs/workshop/figure_8/gridsearch/minibatch/funcopt.yaml --job_name=fig_2

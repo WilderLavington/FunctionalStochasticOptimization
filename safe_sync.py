4d8bb7b3dbc2926b5eb0f92a59fcb8441e70152e
wilderlavington <wilderlavington@gmail.com>
2022-10-07 13:40:18 -0700

    .

--- safe_sync.py
+++ safe_sync.py
@@ -11,20 +11,27 @@ def get_args():
     parser = argparse.ArgumentParser(description='PyTorch Soft Actor-Critic Args')
     # optimization args
     parser.add_argument('--wandb_dir', type=str, default='wandb')
+    return parser.parse_known_args()
 
 def sync_wandb(wandb_dir):
     # get all yaml files in a directory
     yaml_files = []
-    for subdir, dirs, files in os.walk('./'+wandb_dir+'/'):
-        for dir in dirs:
-            if dir.startswith("offline-run"):
-                yaml_files.append(dir)
+    yaml_files = []
+    for subdir, dirs, files in os.walk(wandb_dir):
+        for file in files:
+            filename = os.path.join(subdir, file)
+            print(filename)
+            if "offline-run" in filename:
+                filename = filename.split('/')[:-1]
+                idx = [idx for idx in range(len(filename)) if "offline-run" in filename[idx] ][-1]
+                filename = '/'.join(filename[:idx+1])
+                yaml_files.append(filename)
             else:
                 continue
 
-
+    print(yaml_files)
     for file in yaml_files:
-        command = 'wandb sync '+wandb_dir+'/'+file+ ' --id offlinerun_'+file.split('_')[-1]
+        command = 'wandb sync ./'+file+ ' --id offlinerun_'+file.split('_')[-1]
         print(command)
         exit_status = subprocess.call(command, shell=True)
 
